aea2dcbcb6f9398e6e02a2711c41a72258780683
antoniogomezr <37878434+antoniogomezr@users.noreply.github.com>
2020-12-16 23:40:21 +0000

    Add files via upload

---
+++ node/atpselect.js
@@ -0,0 +1,33 @@
+var oracledb = require('oracledb');
+var dbConfig = require('./dbconfig.js');
+
+// THIS uses Oracle friendly formatting for results 
+oracledb.outFormat = oracledb.OBJECT;
+//
+// // THIS limits the number of rows returned 
+oracledb.maxRows = 50;
+//
+ oracledb.getConnection({ user: dbConfig.dbuser,
+ password: dbConfig.dbpassword, connectString: dbConfig.connectString
+ },
+ function(err, connection)
+ {
+ if (err) { console.error(err); return; } connection.execute(
+ `SELECT channel_desc, TO_CHAR(SUM(amount_sold),'9,999,999,999') SALES$,
+ RANK() OVER (ORDER BY SUM(amount_sold)) AS
+ default_rank,
+ RANK() OVER (ORDER BY SUM(amount_sold) DESC NULLS
+ LAST) AS custom_rank
+ FROM sh.sales, sh.products, sh.customers, sh.times, sh.channels, sh.countries
+ WHERE sales.prod_id=products.prod_id AND sales.cust_id=customers.cust_id
+ AND customers.country_id = countries.country_id AND sales.time_id=times.time_id
+ AND sales.channel_id=channels.channel_id
+ AND times.calendar_month_desc IN ('2000-09', '2000-10')
+ AND country_iso_code='US' GROUP BY channel_desc`,
+ function(err, result)
+{
+if (err) { console.error(err); return; } 
+console.log(result.rows);
+});
+});
+
